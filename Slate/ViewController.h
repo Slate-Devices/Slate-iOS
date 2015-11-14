55b7398dcd0d09f10472d0aa70eb82b22ff12c30
Sam Sheffres <ssheffres@gmail.com>
2015-11-14 17:55:11 +0600

    Initial Commit

---
+++ Slate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Slate
+//
+//  Created by Sam Sheffres on 14/11/15.
+//  Copyright © 2015 Sam Sheffres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
